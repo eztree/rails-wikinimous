fbe08df67e2b0df06007e3eceb31a67c35fa713f
eztree <huienlim@gmail.com>
2021-11-23 15:47:02 +0800

    Basic functionality done, still ugly

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Initializing seeds 🌱"
+
+Article.destroy_all
+
+10.times do |item|
+  puts "Creating a new article"
+  title = "#{Faker::Music.band}, #{Faker::Music.album}"
+  content = Faker::Quote.yoda
+  item = Article.new(title: title, content: content)
+  item.save!
+  puts "Article created!"
+end
+
+puts "End of the seeds ✔"
